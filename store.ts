b3a8a8b0f096a915926207c6837007815ed09756
Atish Bagate <40466303+atishbagate@users.noreply.github.com>
2024-07-08 12:52:53 +0530

    Add files via upload

---
+++ store.ts
@@ -0,0 +1,15 @@
+import { configureStore,Store } from "@reduxjs/toolkit";
+import CounterSlice from "./src/slices/CounterSlice";
+
+
+export const store:Store = configureStore({
+    reducer:{
+        Counter:CounterSlice
+    },
+})
+
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
